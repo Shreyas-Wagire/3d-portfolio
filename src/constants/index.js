e0e692e48ebad90e31aeef23cf60907eb56205ee
Shreyas Wagire <shreyaswagire07sw@gmail.com>
2024-10-27 20:02:55 +0330

    new

---
+++ src/constants/index.js
@@ -0,0 +1,288 @@
+export const navLinks = [
+  {
+    id: 1,
+    name: 'Home',
+    href: '#home',
+  },
+  {
+    id: 2,
+    name: 'About',
+    href: '#about',
+  },
+  {
+    id: 3,
+    name: 'Work',
+    href: '#work',
+  },
+  {
+    id: 4,
+    name: 'Contact',
+    href: '#contact',
+  },
+];
+
+export const clientReviews = [
+  {
+    id: 1,
+    name: 'Prathamesh Sutar',
+    position: 'Video & Photo Editor',
+    img: 'assets/review2.png',
+    review:
+      'Im completely speechless. More than being pieces of my work, its a very beautiful showcase of my skills. The way you choose and aligned these pieces is brilliant. I love the fact that you included descriptions. It is obvious to me that you had taken so much care and thought in this. ',
+  },
+  {
+    id: 2,
+    name: 'Ketan Girmal',
+    position: 'CSE Student',
+    img: 'assets/review1.png',
+    review:
+      'Shreyas`s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional! Fantastic work.',
+  },
+  {
+    id: 3,
+    name: 'Pranav Chavan',
+    position: 'Project Manager at UrbanTech ',
+    img: 'assets/review3.png',
+    review:
+      'I can’t say enough good things about Adrian. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.',
+  },
+  {
+    id: 4,
+    name: 'Gourav Hirwadekar',
+    position: 'Video & Photo Editor',
+    img: 'assets/review4.png',
+    review:
+      'Hello myself gourav hirwadekar.resently i request shreyash to make me the portfolio for my video editing clients so he made a portfolio and I was stunned after looking at portfolio it was an excellent work.',
+  },
+];
+
+export const myProjects = [
+  {
+    title: 'Code with Shreyas - Its a professional website',
+    desc: 'As a software engineer, I specialize in transforming ideas into functional, efficient, and scalable software solutions. My portfolio showcases a variety of projects, from front-end interfaces to full-stack applications, where user experience and innovation come together.',
+    subdesc:
+      'Dive in to see how I build with precision and creativity, whether it’s web development, problem-solving, or feature-rich applications',
+    href: 'https://shreyasport.vercel.app/',
+    texture: '/textures/project/shreyasport.mp4',
+    logo: '/assets/project-logo1.png',
+    logoStyle: {
+      backgroundColor: '#2A1816',
+      border: '0.2px solid #36201D',
+      boxShadow: '0px 0px 60px 0px #AA3C304D',
+    },
+    spotlight: '/assets/spotlight1.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'EditFlow - Portfolio Website of Video Editor', 
+    desc: 'At EditFlow, editing isn’t just a skill—it’s a journey. Each project flows seamlessly from concept to final cut, delivering videos that captivate and move audiences. Ready to see your vision come to life with precision and passion?',
+    subdesc:
+      ' your ideas take shape through cinematic storytelling and meticulous editing. I turn raw footage into polished, impactful visuals that bring your vision to life. Lets craft your story together, frame by frame.',
+    href: 'https://psvideo.vercel.app/',
+    texture: '/textures/project/psvideo.mp4',
+    logo: '/assets/project-logo2.png',
+    logoStyle: {
+      backgroundColor: '#13202F',
+      border: '0.2px solid #17293E',
+      boxShadow: '0px 0px 60px 0px #2F6DB54D',
+    },
+    spotlight: '/assets/spotlight2.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'Foodie. - Hotel Website',
+    desc: 'The restaurants in Hangzhou also catered to many northern Chinese who had fled south from Kaifeng during the Jurchen invasion of the 1120s, while it is also known that many restaurants were run by families.',
+    subdesc:
+      'The restaurants in Hangzhou also catered to many northern Chinese who had fled south from Kaifeng during the Jurchen invasion of the 1120s, while it is also known that many restaurants were run by families.',
+    href: '#',
+    texture: '/textures/project/foodies.mp4',
+    logo: '/assets/project-logo4.png',
+    logoStyle: {
+      backgroundColor: '#60f5a1',
+      background:
+        'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
+      border: '0.2px solid rgba(208, 213, 221, 1)',
+      boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
+    },
+    spotlight: '/assets/spotlight3.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'Dentytech - The Dental App',
+    desc: 'SmilesCare is a comprehensive dental app created to connect patients with dental professionals while simplifying dental care management. Designed with both patients and dental practices in mind,',
+    subdesc:
+      ' SmilesCare offers an intuitive interface, seamless booking options, and access to a personalized dental care plan, making it easy to manage dental health on the go.',
+    href: '#',
+    texture: '/textures/project/vintage.mp4',
+    logo: '/assets/project-logo3.png',
+    logoStyle: {
+      backgroundColor: '#0E1F38',
+      border: '0.2px solid #0E2D58',
+      boxShadow: '0px 0px 60px 0px #2F67B64D',
+    },
+    spotlight: '/assets/spotlight4.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'AnimeWorld - All Anime store',
+    desc: 'AniWorld is a one-stop app for anime lovers, bringing together an extensive anime library, interactive features, and personalized recommendations. From new releases to fan favorites, AniWorld offers streaming, community engagement, and a tailored experience for every fan.',
+    subdesc:
+      'AniWorld combines everything an anime fan would want in one app, from streaming and personalized recommendations to interactive social features. With AniWorld, fans can stay up-to-date on their favorite shows, explore new titles, and connect with a global anime community',
+    href: 'https://anime-site-black.vercel.app/',
+    texture: '/textures/project/anime.mp4',
+    logo: '/assets/project-logo5.png',
+    logoStyle: {
+      backgroundColor: '#1C1A43',
+      border: '0.2px solid #252262',
+      boxShadow: '0px 0px 60px 0px #635BFF4D',
+    },
+    spotlight: '/assets/spotlight5.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+];
+
+export const calculateSizes = (isSmall, isMobile, isTablet) => {
+  return {
+    deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+    deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+    cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
+    reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
+    ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
+    targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+  };
+};
+
+export const workExperiences = [
+  {
+    id: 1,
+    name: 'Framer',
+    pos: 'Lead Web Developer',
+    duration: '2022 - Present',
+    title: "Framer serves as my go-to tool for creating interactive prototypes. I use it to bring designs to  life, allowing stakeholders to experience the user flow and interactions before development.",
+    icon: '/assets/framer.svg',
+    animation: 'victory',
+  },
+  {
+    id: 2,
+    name: 'Figma',
+    pos: 'Web Developer',
+    duration: '2020 - 2022',
+    title: "Figma is my collaborative design platform of choice. I utilize it to work seamlessly with team members and clients, facilitating real-time feedback and design iterations. Its cloud-based.",
+    icon: '/assets/figma.svg',
+    animation: 'clapping',
+  },
+  {
+    id: 3,
+    name: 'Notion',
+    pos: 'Junior Web Developer',
+    duration: '2019 - 2020',
+    title: "Notion helps me keep my projects organized. I use it for project management, task tracking, and as a central hub for documentation, ensuring that everything from design notes to.",
+    icon: '/assets/notion.svg',
+    animation: 'salute',
+  },
+];
